95a832267c2f3e9079d4ee6cfcf7c6c14deaf128
Tim Kittel <tim.kittel@elena-international.com>
2018-08-15 22:53:24 +0200

    add compatibility with numbers

--- src/SymbolicMath.jl
+++ src/SymbolicMath.jl
@@ -1,6 +1,6 @@
 module SymbolicMath
 
-import Base: +, -, *, /, cos, sin, log, exp, abs, real, imag
+import Base: +, -, *, /, cos, sin, log, exp, abs, real, imag, promote_rule, convert
 
 abstract type AbstractTerm end
 struct ExprTerm <: AbstractTerm
@@ -11,13 +11,23 @@ macro ExprTerm(ex)
     ExprTerm(ex)
 end
 
-for op in [:+, :-, :*, :/]
-    @eval $op(t1::ExprTerm, t2::ExprTerm) = Expr(:call, Symbol($(string(op))), t1.ex, t2.ex) |> ExprTerm
-end
-for op in [:cos, :sin, :log, :exp, :abs, :real, :imag]
-    @eval $op(t::ExprTerm) = Expr(:call, Symbol($(string(op))), t.ex) |> ExprTerm
+function createsymbol(s::Symbol)
+    @eval $s = ExprTerm(Symbol($(string(s))))
 end
 
-export @ExprTerm, ExprTerm
+macro symbols(args::Symbol...)
+    createsymbol.(args)
+end
+
+for op in [:+, :-, :*, :/]
+    @eval $op(t1::ExprTerm, t2::ExprTerm) = Expr(:call, Symbol($(string(op))), t1.ex, t2.ex) |> ExprTerm
+    @eval $op(t::ExprTerm, n::Number) = Expr(:call, Symbol($(string(op))), t.ex, n) |> ExprTerm
+    @eval $op(n::Number, t::ExprTerm) = $op(t, n)
+end
+for op in [:cos, :sin, :log, :exp, :abs, :real, :imag]
+    @eval $op(t::ExprTerm) = Expr(:call, Meta.quot(op), t.ex) |> ExprTerm
+end
+
+export @symbols, @ExprTerm, ExprTerm
 
 end # module SymbolicMath
