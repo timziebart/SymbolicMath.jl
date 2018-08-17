81ebfb43d6fa44cd47ec39f2c47e17d233a6c1ef
Tim Kittel <tim.kittel@elena-international.com>
2018-08-17 14:18:32 +0200

    fixed the operators and add equality comparison

--- src/SymbolicMath.jl
+++ src/SymbolicMath.jl
@@ -1,33 +1,51 @@
 module SymbolicMath
 
-import Base: +, -, *, /, cos, sin, log, exp, abs, real, imag, promote_rule, convert
+import Base: +, -, *, /, ^, cos, sin, log, exp, abs, real, imag, convert
+import Base.==
 
 abstract type AbstractTerm end
 struct ExprTerm <: AbstractTerm
     ex
 end
+convert(::Type{ExprTerm}, ex) = ExprTerm(ex)
 
 macro ExprTerm(ex)
     ExprTerm(ex)
 end
 
+function createsymbol(s::Symbol, m::Module)
+    @eval m $s = $(Meta.quot(s)) |> ExprTerm
+end
 function createsymbol(s::Symbol)
-    @eval $s = ExprTerm(Symbol($(string(s))))
+    createsymbol(s, Main)
 end
 
 macro symbols(args::Symbol...)
-    createsymbol.(args)
+    createsymbol.(args, Ref(__module__))
+end
+macro symbol(s::Symbol)
+    createsymbol(s, __module__)
 end
 
-for op in [:+, :-, :*, :/]
-    @eval $op(t1::ExprTerm, t2::ExprTerm) = Expr(:call, Symbol($(string(op))), t1.ex, t2.ex) |> ExprTerm
-    @eval $op(t::ExprTerm, n::Number) = Expr(:call, Symbol($(string(op))), t.ex, n) |> ExprTerm
-    @eval $op(n::Number, t::ExprTerm) = $op(t, n)
+
+# euqality comparison
+==(t1::ExprTerm, t2::ExprTerm) = (t1.ex == t2.ex)
+==(t::ExprTerm, other) = (t.ex == other)
+==(other, t::ExprTerm) = (t == other)
+
+# operator definitions
+binary_opeators = [:+, :-, :*, :/, :^]
+unary_operators = [:cos, :sin, :log, :exp, :abs, :real, :imag]
+
+for op in binary_opeators
+    @eval $op(t1::ExprTerm, t2::ExprTerm) = Expr(:call, $(Meta.quot(op)), t1.ex, t2.ex) |> ExprTerm
+    @eval $op(t::ExprTerm, other) = Expr(:call, $(Meta.quot(op)), t.ex, other) |> ExprTerm
+    @eval $op(other, t::ExprTerm) = Expr(:call, $(Meta.quot(op)), other, t.ex) |> ExprTerm
 end
-for op in [:cos, :sin, :log, :exp, :abs, :real, :imag]
-    @eval $op(t::ExprTerm) = Expr(:call, Meta.quot(op), t.ex) |> ExprTerm
+for op in unary_operators
+    @eval $op(t::ExprTerm) = Expr(:call, $(Meta.quot(op)), t.ex) |> ExprTerm
 end
 
-export @symbols, @ExprTerm, ExprTerm
+export @symbol, @symbols, @ExprTerm, ExprTerm
 
 end # module SymbolicMath
